6050650e86939e35e8675a5670a781e2ed65ba44
heini66 <heini66@gmail.com>
2014-08-03 22:41:20 +0200

    first commit

---
+++ site.mk
@@ -0,0 +1,24 @@
+GLUON_SITE_PACKAGES := \
+	gluon-alfred \
+	gluon-autoupdater \
+	gluon-config-mode \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-luci-admin \
+	gluon-luci-autoupdater \
+	gluon-luci-portconfig \
+	gluon-next-node \
+	gluon-mesh-batman-adv \
+	gluon-mesh-vpn-fastd \
+	gluon-radvd \
+	gluon-status-page \
+	iwinfo \
+	iptables \
+	haveged
+
+
+#DEFAULT_GLUON_RELEASE := 0.4.3
+DEFAULT_GLUON_RELEASE := 0.4.3+0-exp$(shell date '+%Y%m%d')
+# Allow overriding the release number from the command line
+GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+GLUON_PRIORITY ?=0
