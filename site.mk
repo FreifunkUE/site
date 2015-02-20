79cfd13374fd1978a14fc876ef2aedc1c407f2e4
Freifunk Uelzen <FreifunkUE@users.noreply.github.com>
2015-02-20 17:37:46 +0100

    Update site.mk
    Aktuelle Site.mk für Gluon 2014.4

--- site.mk
+++ site.mk
@@ -1,24 +1,32 @@
 GLUON_SITE_PACKAGES := \
-	gluon-alfred \
-	gluon-autoupdater \
-	gluon-config-mode \
-	gluon-ebtables-filter-multicast \
-	gluon-ebtables-filter-ra-dhcp \
-	gluon-luci-admin \
-	gluon-luci-autoupdater \
-	gluon-luci-portconfig \
-	gluon-next-node \
-	gluon-mesh-batman-adv \
-	gluon-mesh-vpn-fastd \
-	gluon-radvd \
-	gluon-status-page \
-	iwinfo \
-	iptables \
-	haveged
+        gluon-mesh-batman-adv-14 \
+        gluon-alfred \
+        gluon-announced \
+        gluon-autoupdater \
+        gluon-config-mode-autoupdater \
+        gluon-config-mode-hostname \
+        gluon-config-mode-mesh-vpn \
+        gluon-config-mode-geo-location \
+        gluon-config-mode-contact-info \
+        gluon-ebtables-filter-multicast \
+        gluon-ebtables-filter-ra-dhcp \
+        gluon-luci-admin \
+        gluon-luci-autoupdater \
+        gluon-luci-private-wifi \
+        gluon-luci-portconfig \
+        gluon-next-node \
+        gluon-mesh-vpn-fastd \
+        gluon-radvd \
+        gluon-status-page \
+        iwinfo \
+        iptables \
+        haveged
 
 
-#DEFAULT_GLUON_RELEASE := 0.4.3
-DEFAULT_GLUON_RELEASE := 0.4.3+0-exp$(shell date '+%Y%m%d')
+#DEFAULT_GLUON_RELEASE := 0.44.0
+#DEFAULT_GLUON_RELEASE := 0.6_exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.63
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
-GLUON_PRIORITY ?=0
+
+GLUON_PRIORITY ?= 0
