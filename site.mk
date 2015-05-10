b6853565b31585aa2d6aca35c0a8ea3a6d1d4de4
Freifunk Uelzen <mail@freifunk-uelzen.de>
2015-05-10 21:08:52 +0200

    Updates für kommendes Release 2015.1 und Signaturkey Marc-Andre fuer Experimental und Testing

--- site.mk
+++ site.mk
@@ -1,9 +1,17 @@
+##	gluon site.mk makefile example
+
+##	GLUON_SITE_PACKAGES
+#		specify gluon/openwrt packages to include here
+#		The gluon-mesh-batman-adv-* package must come first because of the dependency resolution
+
 GLUON_SITE_PACKAGES := \
         gluon-mesh-batman-adv-14 \
         gluon-alfred \
         gluon-announced \
         gluon-autoupdater \
         gluon-config-mode-autoupdater \
+     gluon-setup-mode \
+     gluon-config-mode-core \
         gluon-config-mode-hostname \
         gluon-config-mode-mesh-vpn \
         gluon-config-mode-geo-location \
@@ -12,6 +20,7 @@ GLUON_SITE_PACKAGES := \
         gluon-ebtables-filter-ra-dhcp \
         gluon-luci-admin \
         gluon-luci-autoupdater \
+     gluon-luci-wifi-config \
         gluon-luci-private-wifi \
         gluon-luci-portconfig \
         gluon-next-node \
@@ -22,11 +31,26 @@ GLUON_SITE_PACKAGES := \
         iptables \
         haveged
 
+##	DEFAULT_GLUON_RELEASE
+#		version string to use for images
+#		gluon relies on
+#			opkg compare-versions "$1" '>>' "$2"
+#		to decide if a version is newer or not.
 
-#DEFAULT_GLUON_RELEASE := 0.44.0
-#DEFAULT_GLUON_RELEASE := 0.6_exp$(shell date '+%Y%m%d')
-DEFAULT_GLUON_RELEASE := 0.63
+DEFAULT_GLUON_RELEASE := MA-0.70+exp$(shell date '+%Y%m%d')
+
+
+##	GLUON_RELEASE
+#		call make with custom GLUON_RELEASE flag, to use your own release version scheme.
+#		e.g.:
+#			$ make images GLUON_RELEASE=23.42+5
+#		would generate images named like this:
+#			gluon-ff%site_code%-23.42+5-%router_model%.bin
+
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
 GLUON_PRIORITY ?= 0
+
+# Languages to include
+GLUON_LANGS ?= en de
