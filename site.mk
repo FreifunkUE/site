eab59ae265e3623f3d03252e0ee250c8b9668db7
Freifunk Uelzen <FreifunkUE@users.noreply.github.com>
2015-05-23 10:45:01 +0200

    Add gluon-neighbour-info

--- site.mk
+++ site.mk
@@ -5,31 +5,32 @@
 #		The gluon-mesh-batman-adv-* package must come first because of the dependency resolution
 
 GLUON_SITE_PACKAGES := \
-        gluon-mesh-batman-adv-14 \
-        gluon-alfred \
-        gluon-announced \
-        gluon-autoupdater \
-        gluon-config-mode-autoupdater \
-        gluon-setup-mode \
-        gluon-config-mode-core \
-        gluon-config-mode-hostname \
-        gluon-config-mode-mesh-vpn \
-        gluon-config-mode-geo-location \
-        gluon-config-mode-contact-info \
-        gluon-ebtables-filter-multicast \
-        gluon-ebtables-filter-ra-dhcp \
-        gluon-luci-admin \
-        gluon-luci-autoupdater \
-        gluon-luci-wifi-config \
-        gluon-luci-private-wifi \
-        gluon-luci-portconfig \
-        gluon-next-node \
-        gluon-mesh-vpn-fastd \
-        gluon-radvd \
-        gluon-status-page \
-        iwinfo \
-        iptables \
-        haveged
+	gluon-mesh-batman-adv-14 \
+	gluon-alfred \
+	gluon-announced \
+	gluon-autoupdater \
+	gluon-config-mode-core \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-hostname \
+	gluon-config-mode-mesh-vpn \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-contact-info \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-neighbour-info \
+	gluon-luci-private-wifi \
+	gluon-luci-admin \
+	gluon-luci-autoupdater \
+	gluon-luci-portconfig \
+	gluon-luci-wifi-config \
+	gluon-next-node \
+	gluon-mesh-vpn-fastd \
+	gluon-radvd \
+	gluon-status-page \
+	gluon-setup-mode \
+	iwinfo \
+	iptables \
+	haveged
 
 ##	DEFAULT_GLUON_RELEASE
 #		version string to use for images
