24197f9bf0292c9d7503e18bf99f0d4f2117d66d
Freifunk Uelzen <FreifunkUE@users.noreply.github.com>
2015-05-25 23:46:59 +0200

    Version Change for Stable Build

--- site.mk
+++ site.mk
@@ -38,7 +38,7 @@ GLUON_SITE_PACKAGES := \
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := 0.71
+DEFAULT_GLUON_RELEASE := 0.72
 
 
 ##	GLUON_RELEASE
