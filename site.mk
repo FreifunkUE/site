0e7faccf511afa1e73b2d5ce3f88e72d9c6bfae4
Marc-Andre Alpers <marc-andre@v22015022673823326.yourvserver.net>
2015-07-01 19:21:56 +0200

    Aenderungen für Stable 0.74

--- site.mk
+++ site.mk
@@ -38,7 +38,7 @@ GLUON_SITE_PACKAGES := \
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := 0.73
+DEFAULT_GLUON_RELEASE := 0.74
 
 
 ##	GLUON_RELEASE
