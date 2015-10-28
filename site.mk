986c81c7779732a43889d42d868bf3ce7cd83d57
Marc-Andre Alpers <marc-andre@debian>
2015-10-28 11:52:19 +0100

    Aktuelle Site

--- site.mk
+++ site.mk
@@ -5,32 +5,30 @@
 #		The gluon-mesh-batman-adv-* package must come first because of the dependency resolution
 
 GLUON_SITE_PACKAGES := \
-	gluon-mesh-batman-adv-14 \
-	gluon-alfred \
-	gluon-announced \
-	gluon-autoupdater \
-	gluon-config-mode-core \
-	gluon-config-mode-autoupdater \
-	gluon-config-mode-hostname \
-	gluon-config-mode-mesh-vpn \
-	gluon-config-mode-geo-location \
-	gluon-config-mode-contact-info \
-	gluon-ebtables-filter-multicast \
-	gluon-ebtables-filter-ra-dhcp \
-	gluon-neighbour-info \
-	gluon-luci-private-wifi \
-	gluon-luci-admin \
-	gluon-luci-autoupdater \
-	gluon-luci-portconfig \
-	gluon-luci-wifi-config \
-	gluon-next-node \
-	gluon-mesh-vpn-fastd \
-	gluon-radvd \
-	gluon-status-page \
-	gluon-setup-mode \
-	iwinfo \
-	iptables \
-	haveged
+    gluon-mesh-batman-adv-14 \
+    gluon-alfred \
+    gluon-announced \
+    gluon-autoupdater \
+    gluon-config-mode-autoupdater \
+    gluon-config-mode-contact-info \
+    gluon-config-mode-core \
+    gluon-config-mode-geo-location \
+    gluon-config-mode-hostname \
+    gluon-config-mode-mesh-vpn \
+    gluon-ebtables-filter-multicast \
+    gluon-ebtables-filter-ra-dhcp \
+    gluon-luci-admin \
+    gluon-luci-autoupdater \
+    gluon-luci-portconfig \
+    gluon-luci-wifi-config \
+    gluon-next-node \
+    gluon-mesh-vpn-fastd \
+    gluon-radvd \
+    gluon-setup-mode \
+    gluon-status-page \
+    haveged \
+    iptables \
+    iwinfo
 
 ##	DEFAULT_GLUON_RELEASE
 #		version string to use for images
@@ -38,7 +36,7 @@ GLUON_SITE_PACKAGES := \
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := 0.74
+DEFAULT_GLUON_RELEASE := 0.8-exp$(shell date '+%Y%m%d')
 
 
 ##	GLUON_RELEASE
