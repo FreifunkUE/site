503d71bf99ec930729025c271805e09fc3be4f9d
Marc-Andre Alpers <marc-andre@debian>
2015-11-22 10:22:28 +0100

    MCastrate 6000

--- site.mk
+++ site.mk
@@ -22,6 +22,7 @@ GLUON_SITE_PACKAGES := \
     gluon-luci-autoupdater \
     gluon-luci-portconfig \
     gluon-luci-wifi-config \
+    gluon-luci-private-wifi \
     gluon-next-node \
     gluon-mesh-vpn-fastd \
     gluon-radvd \
